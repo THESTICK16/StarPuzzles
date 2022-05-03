a87dc8ee3829920cd261fd734349241bc809c48b
Vicky Krastev <vickykrastev123@gmail.com>
2022-05-03 13:00:34 -0700

    fixed some merge issue?

--- src/starb/server/PuzzleHandler.java
+++ src/starb/server/PuzzleHandler.java
@@ -5,10 +5,7 @@ import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
 import starb.puzzle.Parser;
-<<<<<<< HEAD
 import starb.puzzle.PuzzleAdt;
-=======
->>>>>>> 906a4c6b97cdc14c2bc40278bb38f4ed9810f6da
 
 import java.io.IOException;
 import java.io.OutputStream;
@@ -21,12 +18,8 @@ public class PuzzleHandler implements HttpHandler{
         String p = uri.getPath();
         String fileName = p.substring(1);
 
-<<<<<<< HEAD
         Parser parse = new Parser();
         String p1 = parse.toString();// getData().toString(); FIXME update to new parser methods
-=======
-        starb.puzzle.Parser parse = new Parser();
->>>>>>> 906a4c6b97cdc14c2bc40278bb38f4ed9810f6da
 
         String p1 = parse.puzzleFileToString(fileName);
 
