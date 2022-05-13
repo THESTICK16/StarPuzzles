bb8f95725f8ea68f204646907b875691f7a9c9cb
Ryan <yorkerj@plu.edu>
2022-05-13 14:22:24 -0700

    .

--- src/starb/client/PuzzleDrawingPanel.java
+++ src/starb/client/PuzzleDrawingPanel.java
@@ -93,6 +93,8 @@ public class PuzzleDrawingPanel extends JComponent{
     private void paintState(Graphics g){
 //        char[][] gameState =
         //TODO add method to clientInteraction to get the game board stateand which spaces are stars or dots, then call paint stars and dots methods
+
+//        g.drawOval(,, cellSide, cellSide);
     }
 
     private void paintStar(Graphics g, int x, int y, int w){
