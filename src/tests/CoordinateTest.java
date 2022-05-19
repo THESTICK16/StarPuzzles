13c524f0dac4d4942f2ce18c2780b46b992cabcc
Ryan <yorkerj@plu.edu>
2022-05-18 22:25:04 -0700

    Improved testing documentation

--- src/tests/CoordinateTest.java
+++ src/tests/CoordinateTest.java
@@ -9,21 +9,33 @@ class CoordinateTest {
 
     Coordinate c = new Coordinate(1,2);
 
+    /**
+     * ensures the toString method returns the x and y values in the format "X,Y"
+     */
     @org.junit.jupiter.api.Test
     void testToString() {
         assertEquals("1,2", c.toString());
     }
 
+    /**
+     *ensures getX returns the proper x coordinate value
+     */
     @Test
     void getX() {
         assertEquals(1,c.getX());
     }
 
+    /**
+     *ensures getY returns the proper y coordinate value
+     */
     @Test
     void getY() {
         assertEquals(2,c.getY());
     }
 
+    /**
+     * ensures that a comparison of two coordinates will return true if the values are equivalent and false if they are different
+     */
     @Test
     void testCompare(){
         Coordinate c2 = new Coordinate(1,2);
